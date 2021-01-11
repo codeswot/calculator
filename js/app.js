ef4d54a2c13eefe3d2b13e1db5a26b195fdc3b20
Mubarak Ibrahim <elmubarak333@gmail.com>
2021-01-11 11:38:22 +0100

    my first commit

---
+++ js/app.js
@@ -0,0 +1,24 @@
+//get pressed button value
+function getInputValue(val){
+    screen =  document.getElementById('calc_screen').innerHTML;
+    if(screen == 0){
+        document.getElementById('calc_screen').innerHTML=val;
+    }
+    else{
+    document.getElementById('calc_screen').innerHTML+=val;
+    }
+}
+//evaluate for answer
+function evaluateAnswer(){
+  var  result =  document.getElementById('calc_screen').innerHTML;
+   var showAns= eval(result);
+
+   document.getElementById('calc_screen').innerHTML=showAns;
+}
+
+//clear screen
+function clearScreen(){
+    
+    document.getElementById('calc_screen').innerHTML='0';
+}
+//test another line
